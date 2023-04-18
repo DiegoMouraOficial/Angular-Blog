e5718fc3d6775db7b0f084547606830b4c96ccd7
Diego Moura Oficial <programmercodeoficial@gmail.com>
2023-04-18 15:28:14 -0300

    criacao dos componentes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "HOMEM DE FERRO  O RETORNO - 2023",
+    "description": "Rumores antigos já diziam que Downey poderia estar tanto na série Coração de Ferro, que estreia em 2023 na Disney+, e o mais recentes apostam que ele possa retornar em algum dos dois próximos filmes dos Vingadores, seja em Dinastia Kang ou Guerras Secretas, filmes que serão o ápice do multiverso na telona.",
+    "photoCover":"https://playcon.gg/app/uploads/2022/11/4033106-marvels-avengers-iron-man.png"
+  },
+  {
+    "id":"2",
+    "title": "Liga da Justiça em 2024",
+    "description": "Rumores sobre um adiamento de Esquadrão Suicida: Mate a Liga da Justiça circularam recentemente quando fontes da Bloomberg informaram que a Warner teria passado o lançamento para o segundo semestre de 2023. A confirmação de hoje, portanto, veio com um atraso ainda maior.Esquadrão Suicida: Mate a Liga da Justiça foi muito criticado pelo público após sua apresentação de 15 minutos durante a State of Play do PlayStation em fevereiro. Além de não gostarem do que viram em relação à jogabilidade, os críticos torceram o nariz para a exigência de conexão com a internet para jogar sozinho ou via cooperação online.",
+    "photoCover":"https://t.ctcdn.com.br/KWyuSKiVzLLisLOttkQ1ThOEJbI=/512x288/smart/filters:format(webp)/i385245.jpeg"
+  },
+  {
+    "id":"3",
+    "title": "Homem Aranha 4 - Lançamento",
+    "description": "Kevin Feige confirma novo filme do Homem-Aranha com Tom Holland no MCU. Embora ainda não apareça no calendário oficial de lançamentos do MCU, Homem-Aranha 4 já foi confirmado por Kevin Feige",
+    "photoCover":"https://cinepop.com.br/wp-content/uploads/2021/12/homem-aranha-4-1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Pantera Negra - Wakanda Para Sempre",
+    "description": "Pantera Negra: Wakanda Para Sempre é a continuação do longa Pantera Negra, da Marvel, dirigido por Ryan Coogler e produzido por Kevin Feige. No filme, o mundo de Wakanda se expande. Após a morte do ator de T'Challa (Chadwick Boseman) o foco de Wakanda Para Sempre são os personagens em volta do Pantera Negra. Rainha Ramonda (Angela Bassett), Shuri (Letitia Wright), M'Baku (Winston Duke), Okoye (Danai Gurira) e as Dora Milage lutam para proteger a nação fragilizada de outros países após a morte de T'Challa. Enquanto o povo de Wakanda se esforça para continuar em frente neste novo capítulo, a família e amigos do falecido rei precisam se unir com a ajuda de Nakia (Lupita Nyong'o), integrante dos Cães de Guerra, e Everett Ross (Martin Freeman). Em meio a isso tudo, Wakanda ainda terá que aprender a conviver com a nação debaixo d'água, Talokan, e seu rei Namor (Tenoch Huerta).",
+    "photoCover":"https://s2.glbimg.com/aale_bzJkmB6q31oUcptqcgBjCc=/0x0:1200x700/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/z/CAFeOATBi4VFAXNyAVVw/pantera-negra-wakanda-forever-1.jpg"
+  }
+
+]
